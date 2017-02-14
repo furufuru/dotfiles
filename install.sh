505d2abb435f7dd857ebe2e8790fceaabe2cac80
Furufuru <mail@furufuru1212.net>
2017-02-14 18:10:16 +0900

    add mpv

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -sf $PWD/i3/config ~/.config/i3/config
 ln -sf $PWD/polybar/config ~/.config/polybar/config
 ln -sf $PWD/polybar/launch.sh ~/.config/polybar/launch.sh
 ln -sf $PWD/termite/config ~/.config/termite/config
+ln -sf $PWD/mpv/mpv.conf ~/.config/mpv/mpv.conf
 for file in '.vim' '.vimrc' '.zshrc'
 do
 	ln -sf $PWD/$file ~/$file
